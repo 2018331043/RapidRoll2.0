0a5530983346205f16afcb80f57ae2422e2ebaf0
2018331043 <mahnur43@student.sust.edu>
2021-02-12 10:25:38 +0600

    Basic Gameplay Achieve

--- Rapid Roll Remastered/Assets/C# Scripts/PlaceHolderMovement.cs
+++ Rapid Roll Remastered/Assets/C# Scripts/PlaceHolderMovement.cs
@@ -6,47 +6,71 @@ public class PlaceHolderMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     private Vector3 startingPosition;
-    public float speed;
+    public float speed=5f;
     public Rigidbody2D rb;
-    private int flag = 0;
+    private float TimePassed;
+    private int flag1 = 1,flag2=1,flag=0;
     void Start()
     {
         startingPosition = transform.position;
         rb = GetComponent<Rigidbody2D>();
+        TimePassed = Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovingTheBall();
+    }
+
+    private void MovingTheBall()
+    {
         if (Input.touchCount > 0)
         {
+            //Debug.Log(Time.deltaTime+"Hello");
+            //Debug.Log("Hello\n");
             Touch firstTouch = Input.GetTouch(0);
             Vector3 position = Camera.main.ScreenToWorldPoint(firstTouch.position);
             position.z = -1f;
             if (position.x > startingPosition.x)
             {
                 //transform.position+=new Vector3(speed*Time.deltaTime,0,0);
-                //transform.Translate(Vector3.right * Time.deltaTime);
-                rb.AddForce(Vector3.right*speed);
+                transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
+                rb.velocity = new Vector3(speed, rb.velocity.y, 0f);
+                /*rb.AddForce(Vector3.right*speed);
+                
                 if (flag == 1)
                 {
-                    rb.AddForce(Vector3.right * speed);
+                    rb.AddForce(Vector3.right * speed*4);
+                    Debug.Log("Eterted flag=1");
                     flag = 2;
                 }
+                flag = 2;*/
+                /*flag1++;
+                rb.AddForce(Vector3.right * speed * (flag2));
+                flag2 = 1;*/
+
             }
-            else if(position.x <= startingPosition.x)
+            else if (position.x <= startingPosition.x)
             {
+                rb.velocity = new Vector3(-speed, rb.velocity.y, 0f);
                 //transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
-                //transform.Translate(Vector3.left * Time.deltaTime, Space.World);
-                rb.AddForce(Vector3.left * speed);
+                //transform.Translate(Vector3.left * Time.deltaTime*speed, Space.World);
+                /*rb.AddForce(Vector3.left * speed);
+                
                 if (flag == 2)
                 {
-                    rb.AddForce(Vector3.left * speed);
+                    rb.AddForce(Vector3.left * speed*4);
+                    Debug.Log("Eterted flag=2");
                     flag = 1;
                 }
+                flag = 1;*/
+                /*flag2++;
+                rb.AddForce(Vector3.left * speed * (flag1));
+                flag1 = 1;*/
             }
             //transform.position = position;
-            
+
         }
     }
 }
---
+++ Rapid Roll Remastered/Assets/MovingDownWIthSpeed.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MovingDownWIthSpeed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private float timePassed;
+    public GameObject platforms;
+    void Start()
+    {
+        timePassed = Time.deltaTime;
+        System.Random rnd = new System.Random();
+        //Instantiate(platforms, new Vector3(Random.Range(1f,5.8f), transform.position.y + 2, -1), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        System.Random rnd = new System.Random();
+        timePassed += Time.deltaTime;
+        if (timePassed >= 2)
+        {
+            timePassed = 0;
+            Instantiate(platforms, new Vector3(Random.Range(1f, 5.8f), transform.position.y - 8, -1), Quaternion.identity);
+        }
+        
+        //Instantiate(platforms, new Vector3((float)rnd.NextDouble()%6, transform.position.y+2, 0), Quaternion.identity);
+    }
+}
